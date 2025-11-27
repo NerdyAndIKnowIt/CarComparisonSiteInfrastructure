395605c5ec73f76e7f6445691f328a608e9f029e
Breanna Chase <breannachase1@gmail.com>
2025-11-27 07:13:46 -0700

    changed the keys in dynamodb

--- infrastructure/dynamodb.tf
+++ infrastructure/dynamodb.tf
@@ -1,16 +1,16 @@
 resource "aws_dynamodb_table" "thatvsthis_table" {
   name         = "ThatVsThisTable"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "make"
-  range_key    = "model"
+  hash_key     = "Make"
+  range_key    = "Model"
 
   attribute {
-    name = "make"
+    name = "Make"
     type = "S"
   }
 
   attribute {
-    name = "model"
+    name = "Model"
     type = "S"
   }
 }
